682e080c29bf27784c32cd6915714bde27a83cdb
makapx <martina.parlavecchio@ctmobi.it>
2023-07-30 14:39:33 +0200

    setup github action

--- next.config.js
+++ next.config.js
@@ -1,5 +1,16 @@
 /** @type {import('next').NextConfig} */
 
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+
+let assetPrefix = ''
+let basePath = '/'
+
+if (isGithubActions) {
+	const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+	assetPrefix = `/${repo}/`
+	basePath = `/${repo}`
+}
+
 const nextConfig = {
 	reactStrictMode: true,
 	env: {
@@ -12,4 +23,8 @@ const nextConfig = {
 	},
 }
 
-module.exports = nextConfig
+module.exports = {
+	...nextConfig,
+	assetPrefix: assetPrefix,
+	basePath: basePath,
+}
