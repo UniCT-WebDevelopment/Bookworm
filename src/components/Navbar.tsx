c4cbe0a42c3eab4777801437586830797e537a73
makapx <martina.parlavecchio@ctmobi.it>
2023-08-01 13:48:09 +0200

    feat: user logout

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,18 +1,38 @@
-import { IconContext } from 'react-icons'
-import { BiUser } from 'react-icons/bi'
-import SwitchTheme from './SwitchTheme'
-import Link from 'next/link'
-import logo from '../../public/logowhite.svg'
-import Searchbar from './Searchbar'
-import Image from 'next/image'
-
+import { IconContext } from 'react-icons';
+import { BiUser, BiLogOutCircle } from 'react-icons/bi';
+import SwitchTheme from './SwitchTheme';
+import Link from 'next/link';
+import logo from '../../public/logowhite.svg';
+import Searchbar from './Searchbar';
+import Image from 'next/image';
+import {createClientComponentClient} from '@supabase/auth-helpers-nextjs';
+import { useEffect, useState } from 'react';
+/**
+ * Return the Navbar component
+ * 
+ * @return {JSX.Element} Navbar component
+ */
 const Navbar = () => {
-	const menuItems = [
-		{
-			name: '',
-			link: '',
-		},
-	]
+	const [isAuthenticated, setIsAuthenticated] = useState(false);
+	const supabase = createClientComponentClient();
+
+	useEffect(() => {
+		const getUser = async () => {
+			const { data: { user }} = await supabase.auth.getUser();
+			
+			if (!user){
+				return;
+			}
+			
+			setIsAuthenticated(true);
+		}
+		getUser();
+	}, [supabase]);
+
+	const handleLogout = () => {
+		setIsAuthenticated(false);
+		supabase.auth.signOut();
+	};
 
 	return (
 		<nav className="navbar bg-primary sticky top-0 z-50 flex justify-between font-semibold h-8">
@@ -23,17 +43,12 @@ const Navbar = () => {
 			</div>
 			<div>
 				<ul className="flex gap-2">
-					{menuItems.map((item, index) => (
-						<li key={index}>
-							<a href={item.link}>{item.name}</a>
-						</li>
-					))}
 					<li>
 						<Searchbar />
 					</li>
 					<li>
 						<button className="btn btn-circle bg-primary-focus hover:bg-accent border-none">
-							<Link href="/profile">
+							<Link href="/login">
 								<IconContext.Provider value={{ size: '2rem', className: 'text-base-100' }}>
 									<BiUser />
 								</IconContext.Provider>
@@ -43,10 +58,24 @@ const Navbar = () => {
 					<li>
 						<SwitchTheme />
 					</li>
+					<li>
+						{isAuthenticated && (
+							<>
+								<button
+									className="btn btn-circle bg-primary-focus hover:bg-accent border-none"
+									onClick={handleLogout}
+								>
+									<IconContext.Provider value={{ size: '2rem', className: 'text-base-100' }}>
+										<BiLogOutCircle />
+									</IconContext.Provider>
+								</button>
+							</>
+						)}
+					</li>
 				</ul>
 			</div>
 		</nav>
 	)
 }
 
-export default Navbar
+export default Navbar;
